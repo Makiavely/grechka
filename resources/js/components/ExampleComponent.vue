090b948e694a7349e2f28f978dc88162492c61c3
Roman <fioletmail@gmail.com>
2021-02-21 18:46:16 +0300

    This is Basic App Design

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,12 +1,309 @@
 <template>
-    <div class="container">
+    <div class="">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Example Header of Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component sadfasdfas fasdfasdfasd fasdfasdf asdf asdf asdf asf asdadsfasdfasd fasdf adsf asdf </div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Header of Component</div>
+
+                    <div class="card-body">
+                        I'm an example component body.
                     </div>
                 </div>
             </div>
